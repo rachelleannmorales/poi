81e3213deffa5c04a05174b1d8dbcaf781e840bb
rachelle <rachelleannmorales@gmail.com>
2019-03-24 01:48:08 +0400

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+      HttpClientModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatTableModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
